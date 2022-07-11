997dc476c1e4c43cc5f9735ea97fa75db2c5a92d
FrazerChard <chard965@gmail.com>
2022-07-11 15:53:43 +0100

    Add files via upload

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const FORTA_CREATE_FUNCTION: string = "function createAgent(uint256 agentId, address owner, string calldata metadata, uint256[] calldata chainIds)";
+export const FORTA_PROXY_CONTRACT: string = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
+export const NETH_DEPLOYER_ADDRESS: string = "0x88dc3a2284fa62e0027d6d6b1fcfdd2141a143b8";
