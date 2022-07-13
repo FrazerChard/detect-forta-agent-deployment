69a08d0c5296e8ef203e710687498a982e1aaee2
FrazerChard <chard965@gmail.com>
2022-07-13 13:44:03 +0100

    Add files via upload

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -1,11 +1,12 @@
 import { FindingType, FindingSeverity,Finding, HandleTransaction, createTransactionEvent } from "forta-agent";
 import agent from "./agent"
+import { createAddress } from "forta-agent-tools/lib/tests";
 import { FORTA_CREATE_FUNCTION, FORTA_PROXY_CONTRACT, NETH_DEPLOYER_ADDRESS } from "./constants"
 
 describe("Monitor Nethermind Agent Deployment Function Calls", () => {
   let handleTransaction: HandleTransaction;
-  const mockAccount: string = "0x6b51cb10119727a5e5ea3538074fb341f56b09cb";
-  const mockAddress: string = "0x7f51eb44623745a5e5ea3538074fb341f56b07ef";
+  const mockAccount = createAddress("0xabc");
+  const mockAddress: string = createAddress("0x123");
 
   beforeAll(() => {
     handleTransaction = agent.handleTransaction;
@@ -33,7 +34,7 @@ describe("Monitor Nethermind Agent Deployment Function Calls", () => {
     it("returns a finding if the Nethermind deployer address calls the createAgent function to the proxy contract", async () => {
       const mockDeployAgent = {
         name: "createAgent",
-        args: {agentId: BigInt("1"), owner: mockAccount, metadata: "", chainIds: [BigInt("1"), BigInt("7")]},
+        args: {agentId: 1, owner: mockAccount, metadata: "", chainIds: [1, 2]},
         address: FORTA_PROXY_CONTRACT,
       }
       const mockTxEvent = createTransactionEvent( { transaction: {from: NETH_DEPLOYER_ADDRESS, to: FORTA_PROXY_CONTRACT}} as any);
